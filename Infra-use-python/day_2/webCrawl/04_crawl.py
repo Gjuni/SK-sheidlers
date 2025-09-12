6ed5f61433c6c70276a7db4ddbbd0e787bed81f1
Gjuni <kmj06178@gmail.com>
2025-09-12 09:35:58 +0900

    [python Infra] day2 fixed print to file

--- Infra-use-python/day_2/webCrawl/04_crawl.py
+++ Infra-use-python/day_2/webCrawl/04_crawl.py
@@ -20,11 +20,15 @@ url = f"https://www.malware-traffic-analysis.net/{year}/index.html"
 
 header_info = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0'}
 
-r = requests.get(url, header_info)
+r = requests.get(url, headers=header_info, verify=False) # Verfy의 경우 HTTPS에서 인증서가 만료되거나 정상적인 인증서가 아닐 때 클롤링이 잘 안되는 문제를 Verify로 해결.
 
 soup = BeautifulSoup(r.text, 'html.parser')
 
 content = soup.select('#main_content > div.content > ul > li > a.main_menu')
 date = soup.select('#main_content > div.content > ul > li > a.list_header')
-for i,j in zip(date, content) :
-    print(f"date : {i.text}, name : {j.text}")
+
+with open('malwares.txt', 'w', encoding='utf-8') as file:
+    for i,j in zip(date, content) :
+        file.write(f"date : {i.text}, name : {j.text}\n")
+        new_url = f"https://www.malware-traffic-analysis.net/{year}/{j.get('href')}"
+        file.write(f"{new_url}\n\n")
